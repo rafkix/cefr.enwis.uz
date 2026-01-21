619e46d1e9cef3e250efd8fbbd49bae42e79d198
rafkix <rafkixuz@gmail.com>
2026-01-21 18:29:35 +0500

    edit code

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import { Header } from "@/components/Header"
-import { Footer } from "@/components/Footer"
+import { Header } from "./Header"
+import { Footer } from "./Footer"
+
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
     const pathname = usePathname()
