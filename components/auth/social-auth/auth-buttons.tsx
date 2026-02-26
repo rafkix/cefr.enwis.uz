6bb4a21a8aca24224288ba4ae6132531df21c74f
rafkix <rafkixuz@gmail.com>
2026-02-26 23:28:49 +0500

    edit

--- components/auth/social-auth/auth-buttons.tsx
+++ components/auth/social-auth/auth-buttons.tsx
@@ -134,11 +134,11 @@ export const TelegramSignInWidget = () => {
     script.src = 'https://telegram.org/js/telegram-widget.js?22'
     script.async = true
     script.setAttribute('data-telegram-login', 'EnwisAuthBot')
-    script.setAttribute('data-size', 'large')
-    script.setAttribute('data-radius', '4')
+    script.setAttribute('data-size', 'medium')
+    script.setAttribute('data-radius', '5')
     script.setAttribute('data-onauth', 'onTelegramAuth(user)')
     script.setAttribute('data-request-access', 'write')
-    script.setAttribute('data-userpic', 'false') // Profil rasmini aniq ko'rsatish
+    script.setAttribute('data-userpic', 'true') // Profil rasmini aniq ko'rsatish
 
     if (telegramWrapperRef.current) {
       telegramWrapperRef.current.innerHTML = ''
